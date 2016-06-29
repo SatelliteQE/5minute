d8015fb50ebdc96edd9d3929b059959201480969
Radovan Drazny <rdrazny@redhat.com>
2016-06-29 12:17:25 +0200

    Testing Travis deploy to PyPi

--- setup.py
+++ setup.py
@@ -3,15 +3,27 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import re
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
+def get_version():
+    VERSIONFILE = path.join('vminute', '__init__.py')
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ ?= ?['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+
 setup(
     name='5minute',
-    version='0.2.1',
+    version=get_version(),
     description='A tool for quick creation and deployment of Openstack machines used for QA testing.',
     long_description=long_description,
     url='https://github.com/SatelliteQE/5minute',
